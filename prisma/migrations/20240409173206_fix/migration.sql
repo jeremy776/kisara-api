c9421cb0fafc2228694c794659f78588040d9006
AnggaaIs <servantangga@gmail.com>
2024-04-10 07:15:22 +0000

    refactor(delete): remove migrations prisma

--- prisma/migrations/20240409173206_fix/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "User" DROP CONSTRAINT "User_discordAccountEmail_fkey";
-
--- AddForeignKey
-ALTER TABLE "User" ADD CONSTRAINT "User_discordAccountEmail_fkey" FOREIGN KEY ("discordAccountEmail") REFERENCES "DiscordAccount"("email") ON DELETE CASCADE ON UPDATE CASCADE;
