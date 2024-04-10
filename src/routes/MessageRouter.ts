61576b6ebac6938c25c7013308e73b4e9dd22191
AnggaaIs <servantangga@gmail.com>
2024-04-10 16:32:26 +0000

    fix(deploy): fix package.json

--- src/routes/MessageRouter.ts
+++ src/routes/MessageRouter.ts
@@ -2,8 +2,10 @@ import { FastifyInstance } from "fastify";
 import { PostBodyMessage } from "../types/message/PostBodyMessage";
 import PostBodyMessageSchema from "../schemas/message/PostBodyMessage.json";
 import GetParamsMessageSchema from "../schemas/message/GetParamsMessage.json";
+import GetParamsMessage2Schema from "../schemas/message/GetParamsMessage_2.json";
 import { prisma } from "../index";
 import { GetParamsMessage } from "../types/message/GetParamsMessage";
+import { GetParamsMessage_2 } from "types/message/GetParamsMessage_2";
 
 export default async function (server: FastifyInstance): Promise<void> {
   server.post<{ Body: PostBodyMessage }>(
@@ -79,8 +81,6 @@ export default async function (server: FastifyInstance): Promise<void> {
         },
       });
 
-      request.log.warn(link_id);
-
       if (!user) {
         return reply.code(404).send({
           statusCode: 404,
@@ -94,6 +94,41 @@ export default async function (server: FastifyInstance): Promise<void> {
         name: "SUCCESS",
         data: { author: { username: user.username, role: user.role }, comments: user.comments },
       });
+    },
+  );
+
+  server.post<{ Params: GetParamsMessage_2 }>(
+    "/:id/:reply_id",
+    {
+      schema: { params: GetParamsMessage2Schema },
+      preHandler: server.rateLimit({
+        max: 45,
+        timeWindow: 60 * 1000,
+      }),
+    },
+    async (request, reply) => {
+      const { id: link_id, reply_id } = request.params;
+
+      const user = await prisma.user.findUnique({
+        where: {
+          link_id,
+        },
+      });
+
+      if (!user) {
+        return reply.code(404).send({
+          statusCode: 404,
+          name: "INVALID_LINK_ID",
+          message: "Oops! It looks like the message link ID you entered doesn't lead anywhere. 🧐",
+        });
+      }
+
+      const comment = await prisma.comment.findUnique({
+        where: {
+          id: reply_id,
+        },
+      });
+      if (!comment) return;
     },
   );
 }
